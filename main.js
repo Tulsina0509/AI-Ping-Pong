f98685b296c99e0d54d2c24f95ceb77be2f52f88
Tulsina0509 <74660837+Tulsina0509@users.noreply.github.com>
2022-06-01 21:01:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,15 +20,50 @@ var ball = {
     dx:3,
     dy:3
 }
+rightWristY = 0;
+rightWristX = 0;
+scoreRightWrist = 0;
+
+Game_status = "";
 
 function setup(){
   var canvas =  createCanvas(700,600);
+  canvas.parent('canvas');
+
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist = results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
+}
+
+function startGame()
+{
+  Game_status = "start";
+  document.getElementById("status").innerHTML = "Game Is Loaded";
 }
 
 
 function draw(){
 
  background(0); 
+ image(video, 0,0,700,600);
 
  fill("black");
  stroke("black");
@@ -37,6 +72,14 @@ function draw(){
  fill("black");
  stroke("black");
  rect(0,0,20,700);
+
+ if(scoreRightWrist > 0.2)
+ {
+   fill("red");
+   stroke("red");
+   circle(rightWristX,rightWristY,30);
+ }
+ 
  
    //funtion paddleInCanvas call 
    paddleInCanvas();
